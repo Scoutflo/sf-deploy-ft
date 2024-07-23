0a687c3e69b52fd0f1ede64c84768773da7e1040
vedant-8680 <146105565+vedant-8680@users.noreply.github.com>
2024-07-24 01:20:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -136,7 +136,7 @@ resource "aws_eks_addon" "vpc-cni" {
 resource "aws_eks_addon" "coredns" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "coredns"
-  addon_version            = "v1.10.1-eksbuild.4" 
+  addon_version            = "v1.10.1-eksbuild.6" 
   tags = {
     "eks_addon" = "coredns"
     "terraform" = "true"
